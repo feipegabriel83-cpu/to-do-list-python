340bb64eedf945c628198fef6524b68e6777fcfc
Felippe Gabriel Santos <feipegabriel.83@gmail.com>
2026-05-30 12:58:41 -0300

    v0.2 - Validação de tarefas duplicadas e melhorias na remoção
    - Adicionada validação para evitar tarefas duplicadas
    - Melhorado tratamento de remoção de tarefas
    - Adicionada mensagem para lista vazia

--- to-do-list-v01.py
+++ to-do-list-v01.py
@@ -3,7 +3,11 @@ from time import sleep
 lista = []
 def adicionar_task():
     adicionar = str(input("Digite uma nova tarefa: ")).strip()
-    lista.append(adicionar)
+    if adicionar in lista:
+        print("Tarefa já existente. Tente novamente!")
+    else:
+        lista.append(adicionar)
+        print("Tarefa adicionada com sucesso!!")
 def remover_task():
     remover = str(input("Qual tarefa deseja remover? ")).strip()
     if remover not in lista:
@@ -12,7 +16,11 @@ def remover_task():
         lista.remove(remover)
         print("Tarefa removida com sucesso!!")
 def listar_task():
-    print(lista)
+    if not lista:
+        print("Sua lista de tarefas está vazia!")
+    else:
+        print("Esta é a sua lista:")
+        print(lista)
 
 listar_task()
 
@@ -30,22 +38,30 @@ while True:
                 [1] - SIM
                 [2] - NÃO """)
         opcao2 = int(input("Digite sua opção: ").strip())
-        if opcao2 == 1:
-            adicionar_task()
-            listar_task()
-        elif opcao2 == 2:
-            continue
+        while opcao2 == 1:
+                adicionar_task()
+                print("""Deseja adicionar outra tarefa?
+                    [1] - SIM
+                    [2] - NÃO """)
+                opcao2 = int(input("Digite sua opção: ").strip())
+                if opcao2 > 2:
+                    print("Opção inválida! Tente Novamente")
     elif opcao == 2:
         remover_task()
-        listar_task()
         print("""Deseja remover outra tarefa?
                        [1] - SIM
                        [2] - NÃO """)
         opcao2 = int(input("Digite sua opção: "))
-        if opcao2 == 1:
-            remover_task()
-        elif opcao2 == 2:
-            continue
+        while opcao2 == 1:
+                remover_task()
+                print("""Deseja remover outra tarefa?
+                    [1] - SIM
+                    [2] - NÃO""")
+                opcao2 = int(input("Digite sua opção: "))
+                if opcao2 > 2:
+                    print("Opção Inválida! Tente Novamente")
+                else:
+                    continue
     elif opcao == 3:
         listar_task()
     elif opcao == 4:
